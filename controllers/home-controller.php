43f47f93ad668616c0e3359d6cdaf7ece897bdc6
MarieSophieGB <mariesophie.gb@gmail.com>
2023-01-23 12:15:16 +0100

    style dark/light, card info

--- controllers/home-controller.php
+++ controllers/home-controller.php
@@ -1,20 +1,44 @@
 <?php
-
-function rss_reader($rss_feed)
+function rss_reader($rss_feed, $console)
 {
-
     $rss = simplexml_load_file($rss_feed);
 
-
     foreach ($rss->channel->item as $item) {
-        echo '<div class="rss-item">';
-        echo '<h3><a href="' . $item->link . '" target="_blank">' . $item->title . '</a></h3>';
-        echo '<p>' . $item->description . '</p>';
-        echo '</div>';
+        // récupérer la date <dc:date>
+        $date = $item->children('dc', true)->date;
+        $date = date('d/m/Y', strtotime($date));
+        
+    echo '
+    <div class="row">
+     <div class="col-sm-12">
+        <div class="card m-3 rounded" >
+            <div class="row g-0">
+                <div class="col-sm-2">
+                    <img src="'.$console.'" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-sm-10">
+                    <div class="card-body">
+                        <h5 class="card-title"><a href="' . $item->link . '" target="_blank">' . $item->title . '</a></h5>
+                        <p class="card-text text-light">' . $item->description . '</p>
+                        <p class="card-text"><small class="text-secondary">' . $date . '</small></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+     </div>
+    </div>
+        ';
     }
 
     return $rss_feed;
 }
+// images consoles 
+$playstation = "https://w0.peakpx.com/wallpaper/342/1021/HD-wallpaper-sony-logo-playstation.jpg"; 
+$xbox = "https://wallpapercave.com/wp/wp10502006.jpg";
+$switch = "http://i.imgur.com/61EZYqD.png";
+$pc = "https://img.lovepik.com/background/20211030/medium/lovepik-computer-circuit-background-mobile-phone-wallpaper-image_400482049.jpg";
+$mobile = "https://w0.peakpx.com/wallpaper/456/555/HD-wallpaper-cool-color-design-designs-flat-material-mobile.jpg";
 
 
-var_dump(rss_reader('https://www.jeuxactu.com/rss/ps4.rss'));
+var_dump(rss_reader('https://www.jeuxactu.com/rss/ps4.rss', $playstation));
+
