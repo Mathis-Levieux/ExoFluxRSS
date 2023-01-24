e0333b10fc5cbed140eca215d2850ac906c6d712
MarieSophieGB <mariesophie.gb@gmail.com>
2023-01-24 16:07:24 +0100

    footer ok, theme light, rss reader

--- views/pages-view.php
+++ views/pages-view.php
@@ -16,7 +16,9 @@ require('../controllers/pages-controller.php');
 </head>
 
 <body>
-
+<header>
+    <?php include('components/header.php') ?>
+</header>
 <?php 
 
         displayPreferencesArticles();
@@ -25,6 +27,7 @@ require('../controllers/pages-controller.php');
 
 
 
+<?php include('components/footer.php') ?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     <script src="../assets/js/script.js"></script>
