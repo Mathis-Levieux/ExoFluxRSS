7033e72b6ede8a5f83e16c11016b3e2ee63afdd7
Mathis Levieux <mathis.levieux1@gmail.com>
2023-01-25 14:13:08 +0100

    début carousel

--- controllers/rss_reader-controller.php
+++ controllers/rss_reader-controller.php
@@ -92,7 +92,6 @@ function displayPreferencesArticles()
     $allArticles = getArticlesInArray();
     $allArticles = deleteDuplicate($allArticles);
     $allArticles = array_slice($allArticles, 0, getUserNbArticleCookie()); // Récupère les articles dans un tableau et supprime les articles en trop
-
     // foreach de $allarticles
     foreach ($allArticles as $article) {
 
@@ -110,6 +109,8 @@ function displayPreferencesArticles()
         // Récupèrer l'image de la console
         $console = $article['console'];
         $consoleimage = "../assets/img/$console.png";
+        displayCarousel(getFirst5InArray($allArticles), $consoleimage); // Affiche le carousel
+
         echo '
         <div class="card mb-3" data-bs-toggle="modal" data-bs-target="#' . $first_word . '">
             <img src="' . $article['image'] . '" class="card-img-top" alt="photo article">
@@ -189,6 +190,7 @@ function displayOnlyOnePage($console, $imgconsole)
     usort($consoleArticles, function ($a, $b) {
         return strtotime($b['date']) - strtotime($a['date']);
     });
+    displayCarousel(getFirst5InArray($consoleArticles), $imgconsole); // Affiche le carousel
     foreach ($consoleArticles as $article) {
 
         // récupérer la date <dc:date>
@@ -260,4 +262,34 @@ function getUserPage() // Affiche la page demandée par l'utilisateur
     } else {
         displayPreferencesArticles();
     }
+}
+
+
+function getFirst5InArray($array)
+{
+    $arrayCarousel = array_slice($array, 0, 5);
+    return $arrayCarousel;
+}
+
+function displayCarousel($array, $imgconsole)
+{
+    echo '
+    <div id="carouselExample" class="carousel slide">
+<div class="carousel-inner">';
+    foreach ($array as $item) {
+        echo '
+    <div class="carousel-item active">
+      <img src="' . $item['image'] . '" class="d-block w-100" alt="...">
+    </div>';
+    }
+    echo '</div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>';
 }
